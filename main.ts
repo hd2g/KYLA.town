4311404434ed8c71def11d70657f3e310f509ce0
hd2g <nagisa.159s.8673.trap@gmail.com>
2023-09-18 13:08:20 +0900

    feat: add hello bot command

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import * as ENV from '~/env.ts'
 import { game } from '~/src/game.ts'
 import * as Bot from '~/src/bot.ts'
+import { defineBotCommand } from '~/src/bot.ts'
 
 const bot = ENV.GATHER_BOT_NAME
 
@@ -16,4 +17,18 @@ game.subscribeToConnection(connected => {
   game.subscribeToEvent('info', console.info)
   game.subscribeToEvent('warn', console.warn)
   game.subscribeToEvent('error', console.error)
+})
+
+defineBotCommand('hello', {
+  game,
+  bot,
+  options: {
+    message: {
+      kind: 'arg',
+      type: 'string',
+    },
+  },
+  call(game, data, { message }) {
+    Bot.chat(game, data, `Hello, ${message}`)
+  },
 })
